e058e2a12445a23720d5004e5e727b69b0668e97
Luccas Oliveira <102065560+luccasfoliveira@users.noreply.github.com>
2023-02-06 09:47:49 -0300

    Create exercicio_03.py

---
+++ exercicio_03.py
@@ -0,0 +1,41 @@
+"""
+Criar uma classe "Veículo" que tenha atributos marca, modelo, ano de fabricação e velocidade,
+e métodos para acelerar e frear.
+"""
+
+
+class Veiculo:
+    def __init__(self, marca, modelo, ano, velocidade):
+        self.marca = marca
+        self.modelo = modelo
+        self.ano = ano
+        self.velocidade = velocidade
+
+    def acelerar(self, valor):
+        if self.velocidade + valor <= 220:
+            self.velocidade += valor
+        else:
+            print('Você está muito rápido')
+            self.velocidade = 220
+
+    def frear(self, valor):
+        if self.velocidade - valor >= 0:
+            self.velocidade -= valor
+        else:
+            self.velocidade = 0
+
+    def mostrar_velocidade(self):
+        print('Velocidade:', self.velocidade)
+
+
+veiculo = Veiculo('GM', 'Celta', '2001', 150.)
+veiculo.mostrar_velocidade()
+
+veiculo.acelerar(80)
+veiculo.mostrar_velocidade()
+
+veiculo.frear(150)
+veiculo.mostrar_velocidade()
+
+veiculo.acelerar(42)
+veiculo.mostrar_velocidade()
