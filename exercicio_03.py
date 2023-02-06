554e9153f6a68256e33ed41b05b4d1ba10361041
Luccas Oliveira <102065560+luccasfoliveira@users.noreply.github.com>
2023-02-06 10:27:44 -0300

    Update exercicio_03.py

--- exercicio_03.py
+++ exercicio_03.py
@@ -12,6 +12,11 @@ class Veiculo:
         self.velocidade = velocidade
 
     def acelerar(self, valor):
+        """
+        :param valor: float
+        :return: incrementa a velocidade,
+        se não houver ultrapassado 220
+        """
         if self.velocidade + valor <= 220:
             self.velocidade += valor
         else:
@@ -19,12 +24,20 @@ class Veiculo:
             self.velocidade = 220
 
     def frear(self, valor):
+        """
+        :param valor: float
+        :return: decrementa a velocidade,
+        se não houver ultrapassado 0
+        """
         if self.velocidade - valor >= 0:
             self.velocidade -= valor
         else:
             self.velocidade = 0
 
     def mostrar_velocidade(self):
+        """
+        :return: valocidade
+        """
         print('Velocidade:', self.velocidade)
 
 
